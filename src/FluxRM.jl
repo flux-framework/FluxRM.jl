98a478258c4a071392c5b187cd73cec924ac1dcc
Valentin Churavy <v.churavy@gmail.com>
2021-02-12 22:25:25 -0500

    add test infrastructure

--- src/FluxRM.jl
+++ src/FluxRM.jl
@@ -2,6 +2,8 @@ module FluxRM
 
 include("api.jl")
 
+export Broker
+
 function version()
     major = Ref{Cint}()
     minor = Ref{Cint}()
@@ -9,6 +11,58 @@ function version()
 
     API.flux_core_version(major, minor, patch)
     Base.VersionNumber(major[], minor[], patch[])
+end
+
+mutable struct Broker
+    handle::Ptr{API.flux_t}
+
+    function Broker(handle::Ptr{API.flux_t})
+        @assert handle != C_NULL
+        this = new(handle)
+        finalizer(this) do broker
+            API.flux_close(broker)
+        end
+        return this
+    end
+end
+Base.unsafe_convert(::Type{Ptr{API.flux_t}}, broker::Broker) = broker.handle
+# flux_fatal_set
+
+function Broker(uri = nothing; flags = 0)
+    if uri === nothing
+        uri = C_NULL
+    end
+    handle = API.flux_open(uri, flags)
+    Libc.systemerror("flux_open", handle == C_NULL)
+    Broker(handle)
+end
+
+function Base.copy(broker::Broker)
+    handle = API.flux_clone(broker)
+    Broker(handle)
+end
+
+function rank(broker::Broker)
+    r_rank = Ref{UInt32}()
+    API.flux_get_rank(broker, r_rank)
+    r_rank[]
+end
+
+function size(broker::Broker)
+    r_size = Ref{UInt32}()
+    API.flux_get_rank(broker, r_size)
+    r_size[]
+end
+
+function Base.getindex(broker::Broker, key::String)
+    str = API.flux_attr_get(broker, key)
+    Base.unsafe_string(str)
+end
+
+function Base.setindex!(broker::Broker, value::String, key::String)
+    err = API.flux_attr_set(broker, key, value)
+    Libc.systemerror("flux_attr_set", err == -1)
+    value
 end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,3 +2,40 @@ using FluxRM
 using Test
 
 @test FluxRM.version() >= v"0.1.0"
+
+@testset "Outside Flux sessions" begin
+    @test_throws SystemError Broker()
+end
+
+flux_available = success(`flux env`)
+
+if !flux_available
+    @error "flux-core is not installed, skipping further tests"
+    exit()
+end
+
+# Make sure keys exist
+@test success(pipeline(`flux keygen`, stderr=devnull)) || true
+
+function flux_broker(size)
+    inp = Pipe()
+    out = Pipe()
+
+    process = run(pipeline(`flux start --size=$size -- bash`, stdin=inp, stdout=out, stderr=stderr), wait=false)
+    close(out.in)
+
+    t = @async write(inp, "printenv FLUX_URI\n")
+    uri = fetch(@async readline(out))
+
+    return uri, process
+end
+
+@testset "Basic" begin
+    uri, fluxp = flux_broker(4)
+    flux = Broker(uri)
+
+    @test parse(Int, flux["size"]) == 4
+    @test_throws SystemError flux["size"] = "5"
+
+    close(fluxp)
+end
