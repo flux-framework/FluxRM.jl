4b15cd6c09e269db5e044bac48c19b5ff2038c5f
Valentin Churavy <vchuravy@users.noreply.github.com>
2021-02-16 21:43:37 -0500

    add test infrastructure and Flux handle (#4)

--- src/FluxRM.jl
+++ src/FluxRM.jl
@@ -2,6 +2,8 @@ module FluxRM
 
 include("api.jl")
 
+export Flux
+
 function version()
     major = Ref{Cint}()
     minor = Ref{Cint}()
@@ -9,6 +11,58 @@ function version()
 
     API.flux_core_version(major, minor, patch)
     Base.VersionNumber(major[], minor[], patch[])
+end
+
+mutable struct Flux
+    handle::Ptr{API.flux_t}
+
+    function Flux(handle::Ptr{API.flux_t})
+        @assert handle != C_NULL
+        this = new(handle)
+        finalizer(this) do flux
+            API.flux_close(flux)
+        end
+        return this
+    end
+end
+Base.unsafe_convert(::Type{Ptr{API.flux_t}}, flux::Flux) = flux.handle
+# flux_fatal_set
+
+function Flux(uri = nothing; flags = 0)
+    if uri === nothing
+        uri = C_NULL
+    end
+    handle = API.flux_open(uri, flags)
+    Libc.systemerror("flux_open", handle == C_NULL)
+    Flux(handle)
+end
+
+function Base.copy(flux::Flux)
+    handle = API.flux_clone(flux)
+    Flux(handle)
+end
+
+function rank(flux::Flux)
+    r_rank = Ref{UInt32}()
+    API.flux_get_rank(flux, r_rank)
+    r_rank[]
+end
+
+function size(flux::Flux)
+    r_size = Ref{UInt32}()
+    API.flux_get_rank(flux, r_size)
+    r_size[]
+end
+
+function Base.getindex(flux::Flux, key::String)
+    str = API.flux_attr_get(flux, key)
+    Base.unsafe_string(str)
+end
+
+function Base.setindex!(flux::Flux, value::String, key::String)
+    err = API.flux_attr_set(flux, key, value)
+    Libc.systemerror("flux_attr_set", err == -1)
+    value
 end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,3 +2,37 @@ using FluxRM
 using Test
 
 @test FluxRM.version() >= v"0.1.0"
+
+@testset "Outside Flux sessions" begin
+    @test_throws SystemError Flux()
+end
+
+flux_available = success(`flux env`)
+
+if !flux_available
+    @error "flux-core is not installed, skipping further tests"
+    exit()
+end
+
+function start_flux(size)
+    inp = Pipe()
+    out = Pipe()
+
+    process = run(pipeline(`flux start --size=$size -- bash`, stdin=inp, stdout=out, stderr=stderr), wait=false)
+    close(out.in)
+
+    t = @async write(inp, "printenv FLUX_URI\n")
+    uri = fetch(@async readline(out))
+
+    return uri, process
+end
+
+@testset "Basic" begin
+    uri, fluxp = start_flux(4)
+    flux = Flux(uri)
+
+    @test parse(Int, flux["size"]) == 4
+    @test_throws SystemError flux["size"] = "5"
+
+    close(fluxp)
+end
