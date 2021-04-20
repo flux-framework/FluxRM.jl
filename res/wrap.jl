41a7d5b214f45bad9220f4f1f6ffdeb8d376f1c1
Valentin Churavy <v.churavy@gmail.com>
2021-04-19 20:58:27 -0400

    add wrappers for hostlist and idset

--- res/wrap.jl
+++ res/wrap.jl
@@ -9,14 +9,31 @@ isdir(header_dir) || error("$header_dir does not exist")
 
 const FLUX_INCLUDE = normpath(header_dir)
 const FLUX_DIR = joinpath(FLUX_INCLUDE, "flux")
-const FLUX_HEADERS = [joinpath(FLUX_DIR, header) for header in ["core.h"]]
 
 args = ["-I$FLUX_INCLUDE"]
 
 options = load_options(joinpath(@__DIR__, "wrap.toml"))
 
 @add_def pid_t
+@add_def UINT_MAX
 
-ctx = create_context(FLUX_HEADERS, args, options)
+ctx = create_context(joinpath(FLUX_DIR, "core.h"), args, options)
+build!(ctx)
 
+general = options["general"]
+general["module_name"] = ""
+general["prologue_file_path"] = ""
+
+general["library_name"] = "libflux_hostlist"
+general["output_file_path"] = "../src/api_hostlist.jl"
+options["codegen"]["library_name"] = "libflux_hostlist"
+
+ctx = create_context(joinpath(FLUX_DIR, "hostlist.h"), args, options)
+build!(ctx)
+
+general["library_name"] = "libflux_idset"
+general["output_file_path"] = "../src/api_idset.jl"
+options["codegen"]["library_name"] = "libflux_idset"
+
+ctx = create_context(joinpath(FLUX_DIR, "idset.h"), args, options)
 build!(ctx)
